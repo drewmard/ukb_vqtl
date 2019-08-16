9b4a750c969cb349b8d29e89f645122a47d66850
drewmard <arm286@cornell.edu>
2019-08-16 16:51:54 -0400

    “init“

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# ukb_vqtl
+# vQTL analysis in UK Biobank
+
+## Linear Modeling of Deviations
+Directory: ./scripts/L-MOD/
+
+
+#### Script 1
+< insert script 1 >
+
+#### Script 1
+< insert script 1 > \
+< insert >
+
+
+
+## UKB pre-processing of blood cell phenotypes
+Directory: ./scripts/GWAS/
+
+
+#### pre-processing
+< insert script 1 >
+
+#### run GWAS
+run_GWAS.sh \
+(this script runs __________)
+
+#### merge diff assoc files together:
+mergeResults.R
+
+#### clump to significant hits, identify top snps, extract from genotype file
+clump_identify_extract.R
---
+++ scripts/GWAS/mergeResults.R
@@ -0,0 +1,59 @@
+library(data.table)
+phenoName='lymphocyte.count.log'
+# for (CHR in 1:22) {
+for (CHR in 1:22) {
+  print(CHR)
+  RELEVANT_COLUMNS=c('SNP','CHR','BP','NMISS','BETA.x','P.x','BETA.y','P.y','REF','ALT')
+  #mean
+  #print('Mean...')
+  df.mean <- fread(paste0('/athena/elementolab/scratch/anm2868/vQTL/UKB/results/ukbb.',CHR,'.',phenoName,'.muGWAS.qassoc'),data.table = F,stringsAsFactors = F)
+  
+  #var
+  #print('Var...')
+  tryCatch({
+    df.var <- read.table(paste0('/athena/elementolab/scratch/anm2868/vQTL/UKB/results/ukbb.',CHR,'.',phenoName,'.vGWAS.auto.R'),stringsAsFactors = F)
+    colnames(df.var) <- c('CHR','SNP','BP','REF',
+                          'BETA','SE','T','P')
+    
+    # freq
+    #print('Freq...')
+    df.freq <- fread(paste0('/athena/elementolab/scratch/anm2868/vQTL/UKB/results/ukbb.',CHR,'.frq'),data.table = F,stringsAsFactors = F)
+    colnames(df.freq)[4] <- 'ALT'
+    
+    #print('Merge...')
+    df.mg <- merge(df.mean,df.var,by=c('SNP','CHR','BP'))
+    df.mg2 <- merge(df.mg,df.freq[,c('SNP','ALT','MAF')])
+    
+  },error=function(e) { print(paste0('ERROR: ',CHR)) })
+  
+  # df.var <- read.table(paste0('/athena/elementolab/scratch/anm2868/vQTL/UKB/results/ukbb.',CHR,'.',phenoName,'.vGWAS.auto.R'),stringsAsFactors = F)
+
+  # colnames(df.var) <- c('CHR','SNP','BP','REF',
+  #                       'BETA','SE','T','P')
+  # 
+  # # freq
+  # print('Freq...')
+  # df.freq <- fread(paste0('/athena/elementolab/scratch/anm2868/vQTL/UKB/results/ukbb.',CHR,'.frq'),data.table = F,stringsAsFactors = F)
+  # colnames(df.freq)[4] <- 'ALT'
+  # 
+  # print('Merge...')
+  # df.mg <- merge(df.mean,df.var,by=c('SNP','CHR','BP'))
+  # df.mg2 <- merge(df.mg,df.freq[,c('SNP','ALT','MAF')])
+  
+  # print('Saving...')
+  if (CHR==1) {
+    df.save <- df.mg2
+  } else {
+    df.save <- rbind(df.save,df.mg2)
+  }
+  
+}
+
+f.save <- paste0('/athena/elementolab/scratch/anm2868/vQTL/UKB/results/ukbb.',phenoName,'.results.txt')
+fwrite(df.save,f.save,col.names = T,row.names = F,na='NA',quote=F,sep='\t')
+# df.save[order(df.save$P.y)[1:30],]
+# 
+# x <- subset(df.save,MAF > 0.1)
+# x[order(x$P.y)[1:30],]
+
+
