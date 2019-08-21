96f1a9e38abf4b3c94d59ea5c4d68169107c9166
Andrew Marderstein <anm2868@cannes.pbtech>
2019-08-21 15:43:40 -0400

    “init“

--- scripts/GWAS/run_GWAS.sh
+++ scripts/GWAS/run_GWAS.sh
@@ -1,7 +1,36 @@
-# run vGWAS/muGWAS on curie.pbtech:
+#!/bin/bash -l
+#SBATCH -J vQTL
+#SBATCH -n 16
+#SBATCH --mem=258G
 
-for i in {1..22}
-do
-PHENO=lymphocyte.count
-sbatch /home/anm2868/scripts/UKB/072019_run_GWAS.slurm.sh $i $PHENO
-done
+CHR=$1
+echo "Chromosome $CHR analysis..."
+
+echo "Activating environment..."
+source activate HLMM
+
+echo "Starting..."
+spack load -r r@3.5.0
+Rscript /home/anm2868/scripts/UKB/061719_Rserve.R
+
+pheno=/athena/elementolab/scratch/anm2868/vQTL/UKB/phenotypes_blood.2.txt
+dir=/home/kulmsc/athena/ukbiobank/calls
+outdir=/athena/elementolab/scratch/anm2868/vQTL/UKB/results
+prefix=ukbb.$CHR
+phenoName=$2 # bmi
+
+# 1 : freq
+echo "MAF measurements..."
+plink --bfile $dir/$prefix --freq --out $outdir/$prefix
+
+# 2 : mean
+echo "muQTL testing..."
+plink --bfile $dir/$prefix --pheno $pheno --pheno-name $phenoName --assoc --out $outdir/$prefix.$phenoName.muGWAS
+
+# 3 : variance
+echo "vQTL testing..."
+myscript=/athena/elementolab/scratch/anm2868/vQTL/UKB/Rscript/vGWAS.R
+# plink --bfile $dir/$prefix --pheno $pheno --pheno-name $phenoName --R $myscript --threads 1 --out $outdir/$prefix.$phenoName.vGWAS
+plink --bfile $dir/$prefix --pheno $pheno --pheno-name $phenoName --R $myscript --threads 1 --out $outdir/$prefix.$phenoName.vGWAS
+
+echo "Complete."
