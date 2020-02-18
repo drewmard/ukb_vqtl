ae257838bbb1eaf0cd4f3064ab67488d6ac3c823
drewmard <arm286@cornell.edu>
2020-02-18 07:31:53 -0500

    “update“

--- scripts/GWAS/subset/run_HLMM_subset_specific.sh
+++ scripts/GWAS/subset/run_HLMM_subset_specific.sh
@@ -29,7 +29,7 @@ START=0
 END=$( wc -l $BIM | cut -f1 -d' ' )
 PHENO=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/GWAS/preprocess/phenotypes_processed.80.NA.txt
 OUT=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/GWAS/HLMM_results/ukbb.$x1.$x2.impute.$phenoName.HLMM_results.txt
-
+rm ${OUT}.models.gz
 # OUT=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/GWAS/HLMM_results/ukbb.$x1.$x2.impute.HLMM_results.tmp.txt
 # START=4980
 # END=5000
