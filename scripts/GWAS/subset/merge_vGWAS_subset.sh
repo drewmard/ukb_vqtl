7d47b0db9b460de62565104537d7597522d86f58
drewmard <arm286@cornell.edu>
2020-03-18 11:00:14 -0400

    “update“

--- scripts/GWAS/subset/merge_vGWAS_subset.sh
+++ scripts/GWAS/subset/merge_vGWAS_subset.sh
@@ -2,13 +2,13 @@
 ID=/athena/elementolab/scratch/anm2868/vQTL/UKB/Neale_GWAS/andrew_copies/subset/ID.impute.txt
 # ID=/athena/elementolab/scratch/anm2868/vQTL/UKB/Neale_GWAS/andrew_copies/subset/ID.impute.txt.tmp # testing!
 
-phenotype=$1
-# phenotype=monocyte.count.rint.ALL # hard input
+phenoName=$1
+# phenoName=monocyte.count.rint.ALL # hard input
 
 #initialize
-outFile=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/vGWAS_subset/ukbb.$phenotype.vGWAS.txt
+outFile=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/vGWAS_subset/ukbb.$phenoName.vGWAS.txt
 rm $outFile
-head -1 /athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/vGWAS_subset/ukbb.1.1.$phenotype.txt > $outFile
+head -1 /athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/vGWAS_subset/ukbb.1.1.$phenoName.txt > $outFile
 
 # loop
 i=0
@@ -22,7 +22,7 @@ do
 
 #  x1=11
 #  x2=29
-#  f=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/vGWAS_subset/ukbb.$x1.$x2.$phenotype.txt
+#  f=/athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/output/vGWAS_subset/ukbb.$x1.$x2.$phenoName.txt
 #  # print to script
 #  if [ $(wc -l $f | cut -f1 -d' ') -gt 5001 ];
 #  then
@@ -33,7 +33,7 @@ do
  if [ ! -f $f ] || [ $(wc -l $f | cut -f1 -d' ') -gt 5001 ]; 
  then
    echo "ERROR: $i ($x1, $x2)"
-   sbatch /athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/scripts/GWAS/subset/run_vGWAS_subset_specific.sh $phenotype $i
+   sbatch /athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/scripts/GWAS/subset/run_vGWAS_subset_specific.sh $phenoName $i
  else
    tail -n +2 $f >> $outFile
  fi
