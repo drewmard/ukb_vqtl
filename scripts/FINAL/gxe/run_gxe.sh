ad2ea34e68d0b188dbe1407dfcfd3739b95b3ae5
Andrew Marderstein <anm2868@cannes.pbtech>
2020-05-11 12:30:21 -0400

    “update“

--- scripts/FINAL/gxe/run_gxe.sh
+++ scripts/FINAL/gxe/run_gxe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH -J gxe
 #SBATCH -n 32
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 
 Rscript /athena/elementolab/scratch/anm2868/vQTL/ukb_vqtl/scripts/FINAL/gxe/GxE_updated.matched.R
